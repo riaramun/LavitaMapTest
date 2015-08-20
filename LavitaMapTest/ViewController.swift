be6a30f941322e17c6534693954bbd064f9cfe23
Admin <123>
2015-08-20 17:33:13 +0300

    Initial Commit

---
+++ LavitaMapTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LavitaMapTest
+//
+//  Created by Admin on 20/08/15.
+//  Copyright © 2015 Lavita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
